9e4828230a345026619184302f1fe1434b83fd4e
hawemily <42003687+hawemily@users.noreply.github.com>
2019-01-27 11:58:01 +0000

    Delete client.py

--- client.py
+++
@@ -1,7 +0,0 @@
-import requests
-import ichackr.py
-
-data = {"time": 2401181530, "color": "Dark", "loadSize"="half", "gender"="female"}
-url = "http://127.0.0.1:5000/createLaundryRequest"
-r = requests.post(url, data=json.dumps(data))
-r.status_code
